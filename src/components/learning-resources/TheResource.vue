5c6f063fba6b1e5dfd1d05892af76a43feaaf6b8
roshanbhw <roshan_bhw@yahoo.com>
2022-01-26 23:14:15 -0600

    rest

---
+++ src/components/learning-resources/TheResource.vue
@@ -0,0 +1,27 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectedTab('stored-resources')" >Stored Resources<base-button>
+        <base-button @click="setSelectedTab('add-resource')" >Add Resource<base-button>
+    </base-card> 
+    <component :is="selectedTab"></component>   
+</template>
+
+<script>
+
+export default {
+  data() {
+      return {
+          selectedTab: 'stored-resources'          
+    };
+  },
+  methods: {
+      setSelectedTab(tab) {
+          this.setSelectedTab = tab;
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
